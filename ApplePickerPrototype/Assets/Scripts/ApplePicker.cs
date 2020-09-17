b8870b540088cd1bb25db1727679d26c4fcabc3f
blk54 <47371275+blk54@users.noreply.github.com>
2020-09-17 01:04:01 -0400

    1

---
+++ ApplePickerPrototype/Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        basketList = new List<GameObject>();
+
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AppleDestroyed()
+    {
+        // destroy all the falling apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+
+        // destroy one of the baskets
+        // get the index of the last basket in basketList
+        int basketIndex = basketList.Count - 1;
+        // get a reference to that basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("_Scene_0 1");
+        }
+    }
+}
---
+++ ApplePickerPrototype/Assets/Scripts/Basket.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collideWith = coll.gameObject;
+        
+        if (collideWith.tag == "Apple")
+        {
+            Destroy(collideWith);
+        }
+
+        //Parese the text of the scoreGT into an int
+        int score = int.Parse(scoreGT.text);
+        score += 100;
+        //Convert int to string
+        scoreGT.text = score.ToString();
+        // track the high score
+        if (score > HighScore.score)
+        {
+            HighScore.score = score;
+        }
+    }
+}
